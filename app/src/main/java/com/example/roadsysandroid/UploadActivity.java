cab89dee21787bfaeeac7f07e453f738d9987db1
rea1GG <455700637@qq.com>
2020-04-24 01:40:36 +0800

    upload实现图片从相册选择，显示在imageView上，main页面1.0

--- app/src/main/java/com/example/roadsysandroid/UploadActivity.java
+++ app/src/main/java/com/example/roadsysandroid/UploadActivity.java
@@ -3,17 +3,22 @@ package com.example.roadsysandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.PopupWindow;
 
 public class UploadActivity extends AppCompatActivity {
-
+    private static final int PHOTO_REQUEST_GALLERY = 2;// 从相册中选择
     private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +49,27 @@ public class UploadActivity extends AppCompatActivity {
         });
         popupWindow.showAsDropDown(v);
     }
+    public void openPicList(View v){
+        Intent intent = new Intent();
+        intent.setType("image/*");// 开启Pictures画面Type设定为image
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PHOTO_REQUEST_GALLERY);
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PHOTO_REQUEST_GALLERY) {
+            // 从相册返回的数据
+            if (data != null) {
+                // 得到图片的全路径
+                Uri uri = data.getData();
+                System.out.println(uri);
+                ImageView imageView = (ImageView)findViewById(R.id.imageView);
+                imageView.setImageURI(uri);
+            }
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
