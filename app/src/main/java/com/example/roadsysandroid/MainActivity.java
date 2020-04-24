e7d9b0eb88426e24d8a3c4d56bb51727a241e992
rea1GG <455700637@qq.com>
2020-04-24 23:38:33 +0800

    终于可以相册选择图片上传到服务器了，淦！！！！（main页面1.0）

--- app/src/main/java/com/example/roadsysandroid/MainActivity.java
+++ app/src/main/java/com/example/roadsysandroid/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.roadsysandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,9 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        verifyStoragePermissions(this);
     }
     public void goToUpload(View v){
         Intent intent = new Intent(MainActivity.this,UploadActivity.class);
         startActivity(intent);
+    }
+    /*
+    获取权限
+ */
+    private  final int REQUEST_EXTERNAL_STORAGE = 1;
+    private  String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE };
+    public  void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
     }
 }
--- app/src/main/java/com/example/roadsysandroid/UploadActivity.java
+++ app/src/main/java/com/example/roadsysandroid/UploadActivity.java
@@ -1,13 +1,23 @@
 package com.example.roadsysandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.icu.lang.UProperty;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
@@ -17,39 +27,115 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
+import android.widget.Toast;
+
+import com.example.roadsysandroid.util.HttpUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.annotation.Retention;
+import java.security.PublicKey;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okio.BufferedSink;
+import okio.BufferedSource;
 
 public class UploadActivity extends AppCompatActivity {
     private static final int PHOTO_REQUEST_CAREMA = 1;// 拍照
     private static final int PHOTO_REQUEST_GALLERY = 2;// 从相册中选择
-//    private File tempFile;
+    //    private File tempFile;
 //    private File cameraSavePath;//拍照照片路径
     private Context context;
+    private String path;
+
+    private Uri uri;
+    /*
+        获取权限
+     */
+    private  final int REQUEST_EXTERNAL_STORAGE = 1;
+    private  String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE };
+    public  void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
-        Button addBtn = (Button)findViewById(R.id.addPic);
+        Button addBtn = (Button) findViewById(R.id.addPic);
+        Button commitBtn = (Button) findViewById(R.id.commitBtn);
+        verifyStoragePermissions(this);
         context = this;
 //        cameraSavePath = new File(Environment.getExternalStorageDirectory().getPath() + "/" + System.currentTimeMillis() + ".jpg");
+        commitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fileUpload(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        System.out.println("onFailure: " + e.getMessage());
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(UploadActivity.this, "上传失败！", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(UploadActivity.this, "上传成功！", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+            }
+        });
 //        addBtn.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
 //                initPopUpWindow(v);
 //            }
 //        });
+
     }
-    public void initPopUpWindow(View v){
-        View view = LayoutInflater.from(context).inflate(R.layout.popup,null,false);
+
+    //弹出选择菜单
+    public void initPopUpWindow(View v) {
+        View view = LayoutInflater.from(context).inflate(R.layout.popup, null, false);
         Button takePictBtn = (Button) view.findViewById(R.id.picture_selector_take_photo_btn);
         Button selectPicBtn = (Button) view.findViewById(R.id.picture_selector_pick_picture_btn);
         Button cancelBtn = (Button) view.findViewById(R.id.picture_selector_cancel_btn);
-        final PopupWindow popupWindow = new PopupWindow(view, ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT,true);
+        final PopupWindow popupWindow = new PopupWindow(view, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
         popupWindow.setTouchable(true);
-        popupWindow.setTouchInterceptor(new View.OnTouchListener(){
+        popupWindow.setTouchInterceptor(new View.OnTouchListener() {
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -66,6 +152,7 @@ public class UploadActivity extends AppCompatActivity {
             }
         });
     }
+
     //相机拍照
 //
     //相册选择    public void takePic(View v){
@@ -82,10 +169,11 @@ public class UploadActivity extends AppCompatActivity {
     //////        startActivityForResult(intent, PHOTO_REQUEST_CAREMA);
     ////        startActivity(intent);
     ////    }
-    public void openPicList(View v){
-        Intent intent = new Intent();
-        intent.setType("image/*");// 开启Pictures画面Type设定为image
-        intent.setAction(Intent.ACTION_GET_CONTENT);
+    //选择相册
+    public void openPicList(View v) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        intent.setType("image/*");// 开启Pictures画面Type设定为image
+//        intent.setAction(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, PHOTO_REQUEST_GALLERY);
 
     }
@@ -96,9 +184,21 @@ public class UploadActivity extends AppCompatActivity {
             // 从相册返回的数据
             if (data != null) {
                 // 得到图片的全路径
-                Uri uri = data.getData();
+                uri = data.getData();
                 System.out.println(uri);
-                ImageView imageView = (ImageView)findViewById(R.id.imageView);
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                System.out.println(filePathColumn);
+                path = Uri.decode(uri.getEncodedPath());
+                if(uri != null){
+                    Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+// 从数据视图中获取已选择图片的路径
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    path = cursor.getString(columnIndex);
+                    cursor.close();
+                }
+                System.out.println(path);
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
                 imageView.setImageURI(uri);
             }
 
@@ -106,4 +206,59 @@ public class UploadActivity extends AppCompatActivity {
 
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+    //上传功能
+    public void fileUpload(Callback callback) {
+
+        // 获得输入框中的路径
+        EditText prInfo = (EditText) findViewById(R.id.prInfo);
+        File file = new File(path);
+        System.out.println(file.getName());
+//        System.out.println(file.getName());
+        OkHttpClient client = new OkHttpClient();
+        // 上传文件使用MultipartBody.Builder
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("file", file.getName(), RequestBody.create(MediaType.parse("text/plain"), file)) // 提交图片，第一个参数是键（name="第一个参数"），第二个参数是文件名，第三个是一个RequestBody
+                .addFormDataPart("userId", "1") // 提交普通字段
+                .addFormDataPart("prInfo", prInfo.getText().toString())
+                .build();
+
+        // POST请求
+        Request request = new Request.Builder()
+                .url("http://192.168.76.1:8080/android/need/upload")
+                .post(requestBody)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+
+    //    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+//    @SuppressLint("NewApi")
+//    public void commitInfo(View v){
+//        StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//
+//        StrictMode.setThreadPolicy(policy);
+//        EditText prInfo = (EditText)findViewById(R.id.prInfo);
+//        OkHttpClient okHttpClient = new OkHttpClient();
+//        MultipartBody.Builder builder = new MultipartBody.Builder();
+//        builder.setType(MultipartBody.FORM);
+////        builder.addPart(Headers.of("Content-Disposition","multipart/form-data;"),RequestBody.create(MediaType.parse("image/jpeg"),new File(uri.toString())));
+//        builder.addFormDataPart("file",path, RequestBody.create(MediaType.parse("image/jpg"),new File(path)));
+//        builder.addFormDataPart("userId","1");
+//        builder.addFormDataPart("prInfo",prInfo.getText().toString());
+//        RequestBody requestBody = builder.build();
+//        Request.Builder reqBuilder = new Request.Builder();
+//        Request request = reqBuilder
+//                .url("http://192.168.76.1:8080/android/need/upload")
+//                .post(requestBody)
+//                .build();
+//        try {
+//            Response response = okHttpClient.newCall(request).execute();
+//            if(response.isSuccessful()){
+//                Toast.makeText(UploadActivity.this,"上传成功！",Toast.LENGTH_SHORT).show();
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
 }
